97daccd7718247bb5bcbc2c32f1c824441c77b3f
Anthony Peng <anthony.peng.2021@gmail.com>
2022-04-20 10:27:51 -0400

    Primitives of one edge in a cell

--- darts/genotype.py
+++ darts/genotype.py
@@ -2,6 +2,17 @@ from collections import namedtuple
 
 Genotype = namedtuple('Genotype', 'normal normal_concat reduce reduce_concat')
 
+PRIMITIVES = [
+    "none",
+    "max_pool_3x3",
+    "avg_pool_3x3",
+    "skip_connect",
+    "sep_conv_3x3",
+    "sep_conv_5x5",
+    "dil_conv_3x3",
+    "dil_conv_5x5",
+]
+
 DARTS_V1 = Genotype(
     normal = [
         ('sep_conv_3x3', 1),
